39f4504a707f3353e8c6d3d6d872e2f2365a34b8
Vu Duc Tiep <vuductiep@live.com>
2023-08-13 22:38:59 +0900

    register function

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth/next"
+import CredentialsProvider from "next-auth/providers/credentials"
+import prismadb from '@/lib/prismadb'
+import {compare } from 'bcrypt'
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password required')
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!user || !user.hashedPassword) {
+          throw new Error('Email does not exist')
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error('Incorrect password')
+        }
+
+        return user
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  // secret: process.env.NEXTAUTH_SECRET
+})
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import Input from "@/components/Input";
+import axios from "axios";
 import { useCallback, useState } from "react";
 
 const Auth = () => {
@@ -10,6 +11,19 @@ const Auth = () => {
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
   }, [])
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }, [email, name, password])
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -46,7 +60,9 @@ const Auth = () => {
               />
             </div>
 
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register} 
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
