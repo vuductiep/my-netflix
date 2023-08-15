9a2d56da938ee1d26620a3d6c47584e5be388b55
Vu Duc Tiep <vuductiep@live.com>
2023-08-15 23:57:47 +0900

    add logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { signOut } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl text-green-500'>Netflix</h1>
+      <h1 className='text-4xl text-green-500'>Netflix</h1>
+      <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
