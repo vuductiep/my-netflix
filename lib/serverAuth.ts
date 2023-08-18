afefc587c0f952248b3ef9142b0dea8b06c304f6
Vu Duc Tiep <vuductiep@gaion.kr>
2023-08-18 16:19:34 +0900

    completed billboard

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
 import prismadb from "@/lib/prismadb"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { getSession, signOut } from 'next-auth/react'
-import { NextPageContext } from 'next'
-import useCurrentUser from '@/hooks/useCurrentUser'
-import Navbar from '@/components/navbar'
-import Billboard from '@/components/Billboard'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
       redirect: {
-        destination: '/auth',
-        permanent: false
-      }
-    }
+        destination: "/auth",
+        permanent: false,
+      },
+    };
   }
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default function Home() {
-  const {data: user} = useCurrentUser()
+  const {data: movie = []} = useMovieList()
+  console.log(movie)
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList data={movie} title={"Trending Now"} />
+      </div>
     </>
-  )
+  );
 }
